d246bb2de512b21bce631dc74da5afd14adb1195
Roman Suturin <r.suturin@gmail.com>
2019-07-07 05:53:08 +0700

    gh-pages

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import 'react-hot-loader';
-import 'normalize.css';
-import 'rc-slider/assets/index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './modules/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- src/utils/formatTime.js
+++
@@ -1,8 +0,0 @@
-const formatTime = (time) => {
-  const minutes = Math.floor(time / 60);
-  const seconds = Math.floor(time - minutes * 60);
-
-  return `${minutes}:${seconds.toString().padStart(2, '0')}`;
-};
-
-export default formatTime;
